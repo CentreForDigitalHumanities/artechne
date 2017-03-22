be0a188a4c01081341d3fec3c4beba2c55f8b252
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2017-03-22 09:28:58 +0100

    Added a switch to include translations in the search

--- js/search.js
+++ js/search.js
@@ -4,5 +4,45 @@
         $("#block-menu-menu-advanced-search .nav a").each(function(i, e) {
             $(this).attr("href", e.href + window.location.search);
         });
+
+        add_translation_switch();
     });
+
+    // Adds a checkbox to switch between transcriptions only or transcription + translation
+    function add_translation_switch() {
+        var translation   = $("#edit-search-api-views-fulltext-translation-wrapper"),
+            transcription = $("#edit-search-api-views-fulltext-transcription-wrapper"),
+            toggle        = $("<input id='incl_trans' type='checkbox'> \
+                <label for='incl_trans'>Include translations</label>");
+
+        translation.after(toggle);
+
+        // Check the query parameters to see which div should be hidden on load
+        if (window.location.search.indexOf("transcription=&") !== -1) {
+            transcription.hide();
+            // Set the checkbox as checked
+            toggle.prop("checked", true);
+        }
+        else {
+            translation.hide();
+        }
+
+        // On change of the checkbox, move the input from translation to transcription and vice versa
+        $("form[id^=views-exposed-form-search-api]").on("change", "#incl_trans", function() {
+            var translation_input   = translation.find("input"),
+                transcription_input = transcription.find("input");
+            if (this.checked) {
+                translation_input.val(transcription_input.val());
+                translation.show();
+                transcription_input.val('');
+                transcription.hide();
+            }
+            else {
+                transcription_input.val(translation_input.val());
+                transcription.show();
+                translation_input.val('');
+                translation.hide();
+            }
+        });
+    }
 })(jQuery);
